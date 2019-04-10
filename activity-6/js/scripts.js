ef58c6d4ed7f1a41471b98f5d307a6e5e644b293
chrobahs <47793145+chrobahs@users.noreply.github.com>
2019-04-09 20:09:19 -0500

    Add files via upload

--- activity-6/js/scripts.js
+++ activity-6/js/scripts.js
@@ -1,62 +1,79 @@
 var messages = [];
-var messageType =  {
+var messageType = {
   out: 'out-message',
   in: 'in-message',
-  unknown: 'unknown-message'};
-var data=[{
-  type: messageType.out,
-  person: 'Skylar',
-  message: 'Hey Cris, what are excited for at Disney?'},
+  unknown: 'unknown-message'
+};
+
+var data = [
   {
-  type: messageType.in,
-  person: 'Cris',
-  message: 'I am the most excited for Epcot!'}
+    type: messageType.out,
+    person: 'Skylar',
+    message: 'Hey Cris, what are you most excited for at Disney?'
+  },
+  {
+    type: messageType.in,
+    person: 'Cristian',
+    message: 'I think I am most excited for Epcot!'
+  }
 ];
-function Message(type, person, message){
-  this.type=type;
-  this.person=person;
-  this.message=message;}
-function createMessageElement(message){
+
+function Message(type, person, message) {
+  this.type = type;
+  this.person = person;
+  this.message = message;
+}
+
+function createMessageElement(message) {
   var messageText = document.createTextNode(
-    message.person +' said: '+message.message);
-  var messageE1 = document.createElement('div');
-    messageE1.appendChild(messageText);
-    messageE1.className = message.type;
-  return messageE1;}
-function addMessageHandler(event){
+    message.person + ' said: ' + message.message
+  );
+
+  var messageEl = document.createElement('div');
+  messageEl.appendChild(messageText);
+  messageEl.className = message.type;
+  return messageEl;
+}
+
+function addMessageHandler(event) {
   var person, type;
-  var messageInputE1=document.getElementById('message-input');
-  var messageConatinerE1=document.getElementById('message-container');
-  switch (event.target.id){
+
+  var messageInputEl = document.getElementById('message-input');
+  var messageContainerEl = document.getElementById('message-container');
+
+  switch (event.target.id) {
     case 'send-button':
-      person: 'Skylar';
+      person = 'Skylar';
       type = messageType.out;
       break;
     case 'reply-button':
-      person: 'Cristian';
-      type=messageType.in;
+      person = 'Cristian';
+      type = messageType.in;
       break;
     default:
-      person: 'Unknown';
-      type: messageType.unknown;
+      person = 'unknown';
+      type = messageType.unknown;
       break;}
-if(messageInputE1.value !=''){
-  var message = new Message(type, person, messageInputE1.value);
-  messages.push(message);
-  var e1= createMessageElement(message);
-  messageContainerE1.appendChild(e1);
-  messageInputE1.value='';}
+
+  if (messageInputEl.value != '') {
+    var message = new Message(type, person, messageInputEl.value);
+    messages.push(message);
+    var el = createMessageElement(message);
+    messageContainerEl.appendChild(el);
+    messageInputEl.value = '';
+  }}
+
+function init() {
+  document.getElementById('send-button').onclick = addMessageHandler;
+  document.getElementById('reply-button').onclick = addMessageHandler;
+  var messageContainerEl = document.getElementById('message-container');
+  for (let j = 0; j < data.length; j++) {
+    const item = data[j];
+    var message = new Message(item.type, item.person, item.message);
+    messages.push(message);
+    var el = createMessageElement(message);
+    messageContainerEl.appendChild(el);
+  }
 }
-function init(){
-document.getElementById('send-button').onclick=addMessageHandler;
-document.getElementById('reply-button').onclick=addMessageHandler;
-var messageContainerE1=document.getElementByID('message-container');
-for (let j=0; j<data.length; j++){
-  const item=data[j];
-var message = new Message(item.type, item.person, item.message);
-messages.push(message);
-var e1 = createMessageElement(message);
-messageContainerE1.appendChild(e1);
-}
-}
+
 init();
